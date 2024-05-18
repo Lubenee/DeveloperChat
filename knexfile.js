8d3a2b993f507723d441004885b3d743fb0206b6
gaylubeoil <Floggy7x@gmail.com>
2024-05-18 21:18:21 +0300

    Start working with postgres.

--- knexfile.js
+++ knexfile.js
@@ -1,1 +1,20 @@
 require("dotenv").config();
+const { Client } = require("pg");
+
+const client = new Client({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+client
+  .connect()
+  .then(() => {
+    console.log('Connected to PostgreSQL database');
+  })
+  .catch((err) => {
+    console.error('Error connecting to PostgreSQL database', err);
+  });
+
