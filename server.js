9d9682ad1b502e816866b6164e725ea5ce4007d1
gaylubeoil <Floggy7x@gmail.com>
2024-05-20 20:42:44 +0300

    Added body parser.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const routers = require('./routers');
 const cors = require("cors");
+const bodyParser = require("body-parser");
 require("dotenv").config();
 
 const app = express();
