f4236e37f284bba2c05ac06c79a0f08ca90c2227
gaylubeoil <Floggy7x@gmail.com>
2024-05-24 21:21:04 +0300

    Bug fix.

--- routers.js
+++ routers.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const userRoutes = require('./controllers/user-controller');
+const postRoutes = require('./controllers/post-controller')
 
 const router = express.Router();
 
 router.use('/user', userRoutes);
+router.use('/post', postRoutes);
 
 module.exports = router;
