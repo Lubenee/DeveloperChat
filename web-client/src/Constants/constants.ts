4cd42f747fd92f8b2fce73ac1bfd6dcfb4850d5f
gaylubeoil <Floggy7x@gmail.com>
2024-05-24 13:37:10 +0300

    Added more endpoints to the constants.

--- web-client/src/Constants/constants.ts
+++ web-client/src/Constants/constants.ts
@@ -1,6 +1,10 @@
 export const LOGIN = "api/user/login";
 export const REGISTER = "api/user/register";
 export const CHECK_TOKEN = "api/user/check-valid-token";
+export const GET_USER_FROM_TOKEN = "api/user";
+export const UPDATE_USERNAME_EMAIL = "api/user/email-username-update";
+export const UPDATE_PASSWORD = "api/user/password-update";
+export const VERIFY_TOKEN = "api/user/check-valid-token";
 
 export const usersEndpoint = "users";
 export const devsEndpoint = "devs";
