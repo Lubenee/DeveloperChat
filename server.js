55a86276cbc735a9d737f7366d3089eda7b68a73
gaylubeoil <Floggy7x@gmail.com>
2024-05-26 11:30:46 +0300

    Implemented simple chat service using socket.io

--- server.js
+++ server.js
@@ -2,20 +2,23 @@
 const express = require('express');
 const routers = require('./routers');
 const cors = require("cors");
+const http = require('http');
 const bodyParser = require("body-parser");
+const initializeChat = require("./services/chat-service");
 require("dotenv").config();
 
 const app = express();
+const server = http.createServer(app);
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
+initializeChat(server);
 
 app.use('/api', routers);
-// Serve static files from the 'uploads' directory
 app.use('/uploads', express.static('./uploads'));
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`DeveloperChat server is running on port ${PORT}`);
 });
 
--- services/chat-service.js
+++ services/chat-service.js
@@ -1,21 +1,37 @@
 const socketIo = require("socket.io");
+const { Server } = require("socket.io")
+require("dotenv").config();
 
 const initializeChat = (server) => {
-  const io = socketIo(server);
+  const io = new Server(server, {
+    cors: {
+      origin: 'http://192.168.100.171:5173',
+      methods: ['GET', 'POST'],
+    }
+  })
 
   io.on("connection", (socket) => {
-    console.log("A user connected");
+    // console.log("A user connected", socket.id);
 
-    socket.on("message", (data) => {
+    socket.on("joinRoom", (data) => {
+      console.log("from join room", data);
+      socket.join(data);
+    })
+
+    socket.on("sendMessage", (data) => {
       // Save the message to the database (PostgreSQL)
       // Emit the message to other users
-      socket.broadcast.emit("message", data);
+      console.log("Sent message:", data.messageInput, '\n');
+      socket.broadcast.emit("receiveMessage", data);
+      //to(data.room)
     });
 
     socket.on("disconnect", () => {
       console.log("A user disconnected");
     });
   });
+
+
 };
 
 module.exports = initializeChat;
