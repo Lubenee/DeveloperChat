ea4ffa08b632f2dee81ee0064c025af81c44f00b
gaylubeoil <Floggy7x@gmail.com>
2024-05-28 15:15:41 +0300

    Added emojies and some more icons to message input.

--- web-client/src/Components/Chat/ChatRoom.tsx
+++ web-client/src/Components/Chat/ChatRoom.tsx
@@ -4,18 +4,25 @@ import LeftSidebar from "./LeftSidebar";
 import { useState, useEffect, FormEvent } from "react";
 const baseUrl = import.meta.env.VITE_SERVER_HOST;
 import { io, Socket } from "socket.io-client";
-import { PrimaryButton } from "../Core/BrandButton";
 import Messages from "./Messages";
+import EmojiPicker, { EmojiClickData } from "emoji-picker-react";
+
+import SentimentSatisfiedAltIcon from "@mui/icons-material/SentimentSatisfiedAlt";
+import ImageOutlinedIcon from "@mui/icons-material/ImageOutlined";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 
 const ChatRoom = () => {
   const [messages, setMessages] = useState<string[]>([]);
   const [messageInput, setMessageInput] = useState<string>("");
   const [socket, setSocket] = useState<Socket | null>(null);
+  const [showSendButton, setShowSendButton] = useState(false);
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+
   const { chatroomId } = useParams();
 
+  //useEffect -> load messages ...
+
   useEffect(() => {
-    // Connect to the Socket.IO server
-    // Specify the connection options
     const newSocket: Socket = io(baseUrl);
     console.log(chatroomId);
     setSocket(newSocket);
@@ -36,6 +43,10 @@ const ChatRoom = () => {
     };
   }, [socket]);
 
+  useEffect(() => {
+    setShowSendButton(messageInput != "" ? true : false);
+  }, [messageInput]);
+
   const sendMessage = (ev: FormEvent) => {
     ev.preventDefault();
     if (!messageInput.trim()) return;
@@ -45,6 +56,12 @@ const ChatRoom = () => {
     }
 
     setMessageInput("");
+  };
+
+  const handleEmoji = (e: EmojiClickData) => {
+    console.log(e);
+    setMessageInput((prev) => prev + e.emoji);
+    setShowEmojiPicker(false);
   };
 
   return (
@@ -59,16 +76,46 @@ const ChatRoom = () => {
         </div>
         <form
           onSubmit={sendMessage}
-          className="bg-gray-200 p-4 sticky bottom-0 w-full">
-          <div className="flex">
-            <input
-              type="text"
-              value={messageInput}
-              onChange={(ev) => setMessageInput(ev.target.value)}
-              className="flex-1 bg-white px-4 py-2 rounded-md"
-              placeholder="Type a message..."
-            />
-            <PrimaryButton type="submit">Send</PrimaryButton>
+          className=" p-4 sticky bottom-0 w-full flex flex-row">
+          <div className="flex-1 rounded-2xl border-2 border-zinc-600 mr-2 pl-2 pr-2">
+            <div className="flex justify-start items-center flex-row">
+              <div
+                className="cursor-pointer"
+                onClick={() =>
+                  setShowEmojiPicker(showEmojiPicker ? false : true)
+                }>
+                <SentimentSatisfiedAltIcon fontSize="medium" />
+              </div>
+              <div className="absolute bottom-16">
+                <EmojiPicker
+                  open={showEmojiPicker}
+                  onEmojiClick={handleEmoji}
+                />
+              </div>
+              <input
+                type="text"
+                value={messageInput}
+                onChange={(ev) => setMessageInput(ev.target.value)}
+                className="flex-1 bg-transparent px-2 py-2 focus:outline-none focus:border-none "
+                placeholder="Type a message..."
+              />
+              {showSendButton ? (
+                <button
+                  type="submit"
+                  className="text-purple-500 font-semibold mr-2 ">
+                  Send
+                </button>
+              ) : (
+                <>
+                  <div className="p-1">
+                    <ImageOutlinedIcon />
+                  </div>
+                  <div className="p-1">
+                    <FavoriteBorderOutlinedIcon />
+                  </div>
+                </>
+              )}
+            </div>
           </div>
         </form>
       </div>
