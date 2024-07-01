42fe37325802ffe9a93b9200fe62bba839d31fbb
gaylubeoil <Floggy7x@gmail.com>
2024-07-01 20:52:42 +0300

    Added front end for edit

--- web-client/src/Components/Users/UsersRow.tsx
+++ web-client/src/Components/Users/UsersRow.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { userId } from "../../types/shared-types";
 import { PrimaryButton } from "../Core/BrandButton";
 import BrandModal from "../Core/BrandModal";
+import EditUserDataForm from "./EditUserDataForm";
 
 interface UserRowProps {
   id: userId;
@@ -12,12 +13,14 @@ interface UserRowProps {
 }
 
 const UserRow = ({ id, name, email, accountType, onDelete }: UserRowProps) => {
-  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-
-  const closeModal = () => {
-    setIsModalOpen(false);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState<boolean>(false);
+  const [isEditModalOpen, setIsEditModalOpen] = useState<boolean>(false);
+  const closeDeleteModal = () => {
+    setIsDeleteModalOpen(false);
   };
-
+  const closeEditModal = () => {
+    setIsEditModalOpen(false);
+  };
   const getAccountTypeLabel = (accountType: number): string => {
     switch (accountType) {
       case 0:
@@ -42,21 +45,42 @@ const UserRow = ({ id, name, email, accountType, onDelete }: UserRowProps) => {
           {getAccountTypeLabel(accountType)}
         </div>
         <div className="py-2 px-4 border-b border-gray-200 flex-1">
-          <PrimaryButton onClick={() => setIsModalOpen(true)}>
+          <PrimaryButton onClick={() => setIsDeleteModalOpen(true)}>
             Delete
+          </PrimaryButton>
+        </div>
+        <div className="py-2 px-4 border-b border-gray-200 flex-1">
+          <PrimaryButton onClick={() => setIsEditModalOpen(true)}>
+            Edit
           </PrimaryButton>
         </div>
       </div>
       <BrandModal
-        isOpen={isModalOpen}
-        onClose={closeModal}
+        isOpen={isDeleteModalOpen}
+        onClose={closeDeleteModal}
         title="Are you sure you want to delete this user?"
         onSubmit={() => {
           onDelete(id);
-          setIsModalOpen(false);
+          setIsDeleteModalOpen(false);
         }}
         submitButtonText="Delete">
         <p>Confirmation message or additional content can go here.</p>
+      </BrandModal>
+      <BrandModal
+        isOpen={isEditModalOpen}
+        onClose={closeEditModal}
+        title="Are you sure you want to delete this user?"
+        onSubmit={() => {
+          onDelete(id);
+          setIsEditModalOpen(false);
+        }}
+        submitButtonText="Save changes">
+        <EditUserDataForm
+          id={id}
+          initialName={name}
+          initialEmail={email}
+          initialAccountType={accountType}
+        />
       </BrandModal>
     </>
   );
